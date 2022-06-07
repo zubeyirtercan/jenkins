4ce9f95eef654dbe51b6efb929d12ac23520dd08
Zubeyir Tercan <zubeyir.tercan@scalefocus.com>
2022-06-07 17:32:58 +0300

    Jenkins file added

---
+++ petclinic.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/g0t4/jgsu-spring-petclinic'
+            }
+        }
+       
+        stage('Test') {
+            steps {
+                bat 'mvnw.cmd test'
+            }
+
+            post {
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                }
+            }
+        }
+        
+        stage('Package') {
+            steps {
+                bat 'mvnw.cmd package'
+            }
+
+            post {
+                success {
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
